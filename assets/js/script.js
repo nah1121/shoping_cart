23625dfaf14394874254cb3f709c61b6e4255431
nah1121 <nahomyeshitla1121@gmail.com>
2023-08-05 01:19:55 +0300

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,107 @@
-let products;
 
+let products;
+let prod;
+let listcarts = [];
+let items = document.getElementById("items");
+let newDiv = document.getElementById("newdiv")
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
 fetch('./products.json')
-  .then(response => response.json())
-  .then(data => {
-    products = data;
+.then(response => response.json())
+.then(data => {
+  products = data;
     displayProducts(data); 
   });
   
-function displayProducts(products) {
+  
+  /* Set the width of the sidebar to 250px (show it) */
+  function openNav() {
+    document.getElementById("mySidepanel").style.width = "70%";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  
+  document.getElementById("chart").addEventListener("click", openNav)
+  document.getElementById("closebtn").addEventListener("click", closeNav)
+  
+  function displayProducts(products) {
   let result = document.getElementById("products");
-
-  for (let x in products) {
+    prod = products
+    
+  products.forEach((value,key) => {
     result.innerHTML += `
         <div class="products">
-            <h1 id="name">${products[x].name}</h1><br>
-            <img src='assets/img/${products[x].image}' width="200px height="200px""/><br>
-            <h3>Price: ${products[x].price}</h6>
-            <button class="ibtn" onclick="atc(${x})">Add To Chart</button>
+            <h1 id="name">${products[key].name}</h1><br>
+            <img src='assets/img/${products[key].image}' width="200px height="200px""/><br>
+            <h3>Price: ${products[key].price}</h6>
+            <button class="ibtn" onclick="addtocart(${key})">Add To Chart</button>
         </div>        
     `;
-  }
+  })
 }
 
-function atc(key){
-  if(products[key] == null){
-      // copy product from list to cart
-      products[key].quantity = 1;
+function addtocart(key){
+  const product = products[key]
+  if(listcarts[key] == null){
+    listcarts.push({
+      ...product,
+      quantity: 1
+    });
   }
-
+  else{
+    listcarts[key].quantity += 1
+  }
+  
   // this function calls a process which adds product to cart menu
   reloadCart();
 }
 
-let items = document.getElementById("items")
 
 // Reload cart menu slide
-function reloadCart(){
-  items.innerHTML = '';
-  let count = 0;
-  let totalPrice = 0;
-  products.forEach((value, key)=>{
-      totalPrice = totalPrice + value.price;
-      count = count + value.quantity;
-      if(value != null){
-          let newDiv = document.createElement('li');
-          newDiv.innerHTML = `
-              <div><img src="assets/img/products/${value.image}"/></div>
-              <div>${value.name}</div>
-              <div>${value.price.toLocaleString()}</div>
-              <div>
-                  <button onclick="updateQuantity(${key}, ${value.quantity - 1})">-</button>
-                  <div class="count">${value.quantity}</div>
-                  <button onclick="updateQuantity(${key}, ${value.quantity + 1})">+</button>
-              </div>`;
-              items.appendChild(newDiv);
-      }
-  })
-  // total.innerText = totalPrice.toLocaleString();
-  // quantity.innerText = count;
+function reloadCart() {
+
+    
+    newDiv.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listcarts.forEach((value, key)=>{
+        totalPrice += value.price * value.quantity;
+        count += value.quantity;
+        if(value != null){
+            newDiv.innerHTML += `
+            <div id="conts">
+            <div id="conts1">
+            <div id="imgs"><img src="assets/img/${value.image}"/></div>
+            <div id="names"><h1>${value.name}</h1></div>
+            <div id="pric">
+                <h2>${value.price} * ${value.quantity}</h2>
+                <div id="btnss">
+                  <button onclick="updateQuantity(${key}, ${value.quantity - 1})" id="inc">-</button>
+                  <div class="count" id="cnt">${value.quantity}</div>
+                  <button onclick="updateQuantity(${key}, ${value.quantity + 1})" id="dec">+</button>
+                </div>
+            </div>
+            </div>
+            
+            </div>`;
+            }
+            total.innerText = totalPrice.toLocaleString();
+            quantity.innerText = count;
+    })
+    
 }
 
 // Updates the product quantity inside the cart menu
 function updateQuantity(key, quantity){
-  if(quantity == 0){
-      delete products[key];
-  }else{
-      products[key].quantity = quantity;
-      products[key].price = quantity * prod[key].price;
-  }
-  reloadCart();
+    if(quantity == 0){
+        delete listcarts[key];
+    }else{
+        listcarts[key].quantity = quantity;
+        
+    }
+    
+    reloadCart();
 }
-
-
-
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "60%";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
-
-document.getElementById("chart").addEventListener("click", openNav)
-document.getElementById("closebtn").addEventListener("click", closeNav)
